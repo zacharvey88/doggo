021a4098bdd09f1e5e951ab7f219e4028519535b
Natalie <natchc@gmail.com>
2024-05-21 13:45:33 +0100

    sign in and sign up pages

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -5,6 +5,7 @@ import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
+import AuthProvider from '@/src/providers/AuthProvider';
 
 import { useColorScheme } from '@/src/components/useColorScheme';
 
@@ -50,10 +51,13 @@ function RootLayoutNav() {
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <AuthProvider>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
         <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
       </Stack>
+      </AuthProvider>
     </ThemeProvider>
   );
 }
