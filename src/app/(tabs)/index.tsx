55f41d9085dd60b3e9cd7ee7e58d0f66d37cca20
Luke Berry <lukeberry12345@gmail.com>
2024-05-21 11:20:17 +0100

    added DogGo logo and made a button that links to the search page

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,10 +1,21 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Image, Pressable } from 'react-native';
 import { Text, View } from '@/src/components/Themed';
+import { Link } from 'expo-router';
 
 export default function TabHome() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Home</Text>
+      <Image style={styles.image}source={require("../../../assets/images/logo.png")}/>
+
+    <Link href="/search" asChild>
+      <Pressable style={styles.button}>
+        <Text style={styles.buttonText}>
+          Start Planning Your Trip
+        </Text>
+      </Pressable>
+    </Link>
+    
+    
     </View>
   );
 }
@@ -15,13 +26,30 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  title: {
-    fontSize: 20,
+  button: {
+    marginTop:40,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 10,
+    elevation: 3,
+    backgroundColor: 'rgb(1,130,200)',
+  },
+  buttonText: {
+    fontSize: 16,
+    lineHeight: 21,
     fontWeight: 'bold',
+    letterSpacing: 0.25,
+    color: 'white',
   },
   separator: {
     marginVertical: 30,
     height: 1,
     width: '80%',
   },
+  image: {
+    height:"50%",
+    width:"80%",
+  }
 });
