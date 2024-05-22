461a8346a8d818aa98bc259de014846abef011ec
Natalie <natchc@gmail.com>
2024-05-22 14:29:10 +0100

    edit profile in separate page

--- .expo/types/router.d.ts
+++ .expo/types/router.d.ts
@@ -3,7 +3,7 @@ import type { ReactNode } from 'react';
 import type { TextProps, GestureResponderEvent } from 'react-native';
 
 export namespace ExpoRouter {
-  type StaticRoutes = `/` | `/(tabs)` | `/_sitemap` | `/airlines` | `/modal` | `/profile` | `/search` | `/sign-in` | `/sign-up` | `/trips`;
+  type StaticRoutes = `/` | `/(profile)` | `/(tabs)` | `/_sitemap` | `/airlines` | `/modal` | `/search` | `/sign-in` | `/sign-up` | `/trips` | `/update`;
   type DynamicRoutes<T extends string> = never;
   type DynamicRouteTemplate = never;
 
