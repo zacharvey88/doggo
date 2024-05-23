5d19d3965540494484fa7957bf25e635d7281a66
JJamC <jaycarroll97@live.co.uk>
2024-05-23 14:01:46 +0100

    removed comments

--- src/app/icons/Vets.tsx
+++ src/app/icons/Vets.tsx
@@ -111,22 +111,3 @@ const styles = StyleSheet.create({
 
 
 
-
-// 10:50
-// <View style={styles.container}>
-//       <FlatList
-//         data={places}
-//         keyExtractor={(item) => item.id}
-//         renderItem={({ item }) => (
-//           <View style={styles.placeItem}>
-//             <Text style={styles.placeName}>{item.displayName.text}</Text>
-//             <Text style={styles.placeAddress}>{item.formattedAddress}</Text>
-//             <Text style={styles.placeGoogleMapsUri}>{item.googleMapsUri}</Text>
-//             <Text style={styles.placeType}>{item.primaryTypeDisplayName.text}</Text>
-//             <Text style={styles.placeLocation}>
-//               Location: {item.location.latitude}, {item.location.longitude}
-//             </Text>
-//           </View>
-//         )}
-//       />
-//     </View>
