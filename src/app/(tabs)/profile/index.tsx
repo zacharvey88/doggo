db43da8076dfaeab81bfa8210e5c46395d6fbc37
Natalie <natchc@gmail.com>
2024-05-29 09:03:33 +0100

    fix extra import

--- src/app/(tabs)/profile/index.tsx
+++ src/app/(tabs)/profile/index.tsx
@@ -4,7 +4,6 @@ import {
   Pressable,
   StyleSheet,
   TouchableOpacity,
-  SafeAreaView,
 } from "react-native";
 import { useEffect, useState } from "react";
 import Account from "@/src/components/Account";
